038a03682bd06d9fe0586d620653529ef2ddcc37
Peter <peterjjones98@gmail.com>
2021-04-28 12:22:19 -0700

    Initial Commit

---
+++ Code/BotSettings.py
@@ -0,0 +1,29 @@
+settings = {
+
+    "CVV" : "",
+    "RTIME" : 1, #Must be a whole number between 0-3
+    "DRIVER" : r"ChromeWebDriver\chromedriver.exe",
+    "LINKS" : r"ProductLinks.txt",
+    "SKIPSIGNIN" : True, #set this to False if you want it to ask for sign in at the start.
+
+}
+
+
+xPaths = {
+
+    "signIn" : '//*[@id="app"]/header/div[1]/div[4]/div[1]/div[1]',
+    "addToCart" :'//*[@id="ProductBuy"]/div/div[2]/button',
+    "noThanks" : '//*[@id="modal-intermediary"]/div/div/div/div[3]/button[1]', #possible
+    "checkout" : '//*[@id="modal-intermediary"]/div/div/div[2]/div[2]/button[2]',
+    "notInterested" : '//*[@id="Popup_Masks"]/div/div/div[3]/div[2]/button[1]', #possible
+    "secureCheckout" : '//*[@id="app"]/div[1]/section/div/div/form/div[2]/div[3]/div/div/div[3]/div/button',
+    "continue" : '//*[@id="app"]/div/section/div/div/form/div[2]/div[1]/div/div[2]/div/div[3]/button',
+    "cVV" : '//*[@id="app"]/div/section/div/div/form/div[2]/div[1]/div/div[3]/div/div[2]/div[1]/div[2]/div[3]/input',
+    "review" : '//*[@id="app"]/div/section/div/div/form/div[2]/div[1]/div/div[3]/div/div[3]/button',
+    "retype": '//*[@id="app"]/div/div[2]/div[2]/div[1]/input',
+    "place" : '//*[@id="btnCreditCard"]',
+    "email": '//*[@id="labeled-input-signEmail"]',
+    "password" : '//*[@id="labeled-input-password"]',
+    "signedIn" : '//*[@id="myaccount"]/a/div[2]'
+
+}
---
+++ Code/ScanPages.py
@@ -0,0 +1,143 @@
+import time, platform, sys, subprocess, os, requests, selenium, random, lxml, asyncio
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from BotSettings import settings, xPaths
+from lxml import html, etree
+
+
+#Driver Initialize------------------------------------
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+linkFile = settings["LINKS"]
+driver = webdriver.Chrome(options=chrome_options, executable_path=settings["DRIVER"])
+
+action = ActionChains(driver)
+
+waitFor = WebDriverWait(driver, 300) #Max time the driver will wait for you to sign in
+waitCVV = WebDriverWait(driver, 20)
+
+with open(linkFile) as linkReader:
+    productLinks = [link.replace("\n", "") for link in linkReader]
+    linkReader.close()
+
+#----------------------Web bot Class------------------------------
+
+class WebBot:
+
+    def __init__(self):
+        self.refreshing = False
+
+    #Itterates through links and sends HTTP requests to see if each product link is available.
+    #It will return True if a product is in stock. This will send Selenium to the product link that is in stock.
+    async def checkLinks(self):
+        
+        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+        for link in productLinks:
+            response = requests.get(link, headers=headers)
+            root = lxml.html.fromstring(response.content)
+            button = root.xpath(xPaths["addToCart"])
+            if len(button) > 0:
+                print("Item Found. IN STOCK")
+                return link
+            else:
+                print("Not Available")
+            await asyncio.sleep(random.randint(settings["RTIME"], 3))
+        
+        return False
+
+    #adds the found item to cart and will dismiss additional offerings.
+    def addToCart(self, link):
+        driver.get(link)
+        driver.find_element_by_xpath(xPaths["addToCart"]).click()
+        time.sleep(1)
+
+        try:
+            driver.find_element_by_xpath(xPaths["noThanks"]).click()
+            time.sleep(.5)# takes in account for CSS animation
+            driver.find_element_by_xpath(xPaths["checkout"]).click()
+        except:
+            pass
+
+        self.checkForOffer()
+        driver.find_element_by_xpath(xPaths["secureCheckout"]).click()
+        try:
+            self.buyItem()
+        except:
+            print(f"There was an issue trying to purchase an item.\n{link}")
+
+    def checkForOffer(self):
+        time.sleep(.5)
+        try:
+            driver.find_element_by_xpath(xPaths["notInterested"]).click()
+        except:
+            pass
+
+    def buyItem(self):
+        cvv = settings["CVV"]
+        foundCVV = waitCVV.until(EC.url_contains("/shop/checkout"))
+        cvvField = driver.find_element_by_xpath(xPaths["cVV"])
+        cvvField.click()
+        cvvField.send_keys(cvv)
+        time.sleep(.1)
+        action.key_up(cvvField)
+        time.sleep(.1)
+        driver.find_element_by_xpath(xPaths["place"]).click()
+        time.sleep(10)
+
+
+    #logs in and keeps session open by refreshing every 5 seconds.
+    def startSession(self):
+
+        if settings["SKIPSIGNIN"]:
+            return True
+
+        driver.find_element_by_xpath(xPaths["signIn"]).click()
+        loggedIn = waitFor.until(EC.title_contains("Computer Parts, PC Components, Laptop Computers, LED LCD TV, Digital Cameras and more - Newegg.com"))
+
+        if loggedIn:
+            return True
+        else:
+            return False
+    
+    #keeps the session active by refreshing the page at random intervals
+    async def refreshSession(self):
+        print("Refresh was called.")
+        self.refreshing = True
+        await asyncio.sleep(random.randint(1,200))
+        driver.refresh()
+        print("refreshed")
+        self.refreshing = False
+
+    #calls all methods in correct order
+    async def startBot(self):
+        driver.get("https://www.newegg.com/")
+        session = self.startSession()
+        if session:
+            while True: #while items not in stock
+                
+                #coroutines
+                if self.refreshing == False: #if not currently refreshing
+                    refreshTask = asyncio.create_task(self.refreshSession())
+                    refresh = refreshTask #no return value there for no await
+
+                linkTask = asyncio.create_task(self.checkLinks())
+                
+                link = await linkTask
+                
+                if link:
+                    self.addToCart(link)
+                    
+        else:
+            print("Unable to start session.")
+
+eggBot = WebBot()
+asyncio.run(eggBot.startBot())
+
+
+
+
+
