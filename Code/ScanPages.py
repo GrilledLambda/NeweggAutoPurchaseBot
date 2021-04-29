802e769a60d59f04de76536644cdcc9d109792c5
Peter <76958651+Peterjjones98@users.noreply.github.com>
2021-04-29 10:27:29 -0700

    Update ScanPages.py

--- Code/ScanPages.py
+++ Code/ScanPages.py
@@ -37,7 +37,7 @@ class WebBot:
         
         headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
         for link in productLinks:
-            response = requests.get(link, headers=headers)
+            response = requests.get(link, headers=headers, timeout=10)
             root = lxml.html.fromstring(response.content)
             button = root.xpath(xPaths["addToCart"])
             if len(button) > 0:
